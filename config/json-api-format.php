f46780fd63250a1538489348ea491476ebdccacf
Oscar Cabrera <o.cabrera@cyberpuerta.mx>
2024-11-09 08:41:31 -0600

    ✨ feat: use kebab-case for model name generation ✨ Add CI tools and update config files ✨ feat: add Analyzer tool for CI analysis with modified files only 🐛 fix: ensure default route method is 'read' if not provided in config 🔧 chore: add configurable route method using `config('json-api-format.route_method')` 🔧 chore: add workflows for PHPStan, Pint, and PhpMD 📦 build(composer.json): add new dev dependencies and scripts for PHPStan, Pint, and PhpMD 📚 docs(json-api-pagination-transformer): update docblock return type for toArray method 💎 style(config/json-api-format.php): add strict types declaration 💎 style(JsonApiFormatPaginateServiceProvider): add strict types declaration and cleanup comments 💎 style(EntityResourceTransformer): add constructor with method and modelName properties 💎 style(JsonApiPaginationTransformer): add strict types declaration and rename variable 💎 style(JsonApiResourceTransformer): add getAttributes method and update toArray return type 🔨 refactor: introduce `$modelName` and `$method` properties for better encapsulation

--- config/json-api-format.php
+++ config/json-api-format.php
@@ -1,8 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     /*
      * The name of the macro that is added to the Eloquent query builder.
      */
     'method_name' => 'getFields',
+
+    /*
+     * The name of the route method that should be used.
+     */
+    'route_method' => 'read',
 ];
--- src/Transformers/EntityResourceTransformer.php
+++ src/Transformers/EntityResourceTransformer.php
@@ -1,24 +1,42 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Oscabrera\JsonApiFormatPaginate\Transformers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
 use ReflectionClass;
 use ReflectionException;
-use Str;
 
 /**
  * Class JsonApiPaginationTransformer
  *
  * This class represents a JsonApiPaginationTransformer that
+ *
+ * @property object{id?: int|string} $resource
  */
 class EntityResourceTransformer extends JsonResource
 {
+    private string $modelName;
+    private string $method;
+
+    /**
+     * @param array<int|string, mixed>|object $resource
+     */
+    public function __construct(array|object $resource, ?string $method = null, ?string $modelName = null)
+    {
+        parent::__construct($resource);
+        $this->method = $method ?? $this->getMethod();
+        $this->modelName = $modelName ?? $this->defineModelName();
+    }
+
     /**
      * Transform the resource collection into an array.
      *
      * @return array<int|string, mixed>
+     *
      * @throws ReflectionException
      */
     public function toArray(Request $request): array
@@ -33,16 +51,30 @@ class EntityResourceTransformer extends JsonResource
      * Retrieves the links for the resource.
      *
      * @return array{self: string} The links for the resource.
-     * @throws ReflectionException
      */
     private function getLinks(): array
     {
+        return [
+            'self' => route($this->modelName . '.' . $this->method, ['id' => $this->resource->id ?? '']),
+        ];
+    }
+
+    /**
+     * Gets the method used for handling JSON API requests.
+     * If no configuration is provided, the default method is 'read'.
+     */
+    private function getMethod(): string
+    {
+        return strval(config('json-api-format.route_method', 'read'));
+    }
+
+    /**
+     * Gets the name of the model, use resource name as default.
+     */
+    private function defineModelName(): string
+    {
         $name = (new ReflectionClass($this->resource))->getShortName();
         $str = new Str();
-        $modelName = $str::plural($str::snake($name));
-
-        return [
-            'self' => route($modelName . '.read', ['id' => $this->resource->id]),
-        ];
+        return $str::plural($str::kebab($name));
     }
 }
